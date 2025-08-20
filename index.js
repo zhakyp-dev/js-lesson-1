46df4dbff3871a0588a13b446cd57ccbf82640ce
Jakyp-2005 <jakypjumagulov80@gmail.com>
2025-08-20 16:57:36 +0600

    lesson-1

---
+++ index.js
@@ -0,0 +1,57 @@
+// переменные - var, const, let
+
+// var name = "Zhakyp";
+// let age = 50;
+// const from = "Bishkek";
+
+// from = "Osh"
+// age = 60
+// name = "Jonh"
+
+// var name = "Jonh";
+// var name = "Alex"
+
+// let age = 60;
+
+// const from = "Osh"
+
+// console.log(name);
+// console.log(age);
+// console.log(from);
+
+// ctrl + /
+
+const name = "Zhakyp";
+const surname = "Zhumagulov";
+let age = 30;
+let from = "Bishkek";
+
+console.log(name);
+console.log(surname);
+console.log(age);
+console.log(from);
+
+let a = 5;
+let b = 10;
+const resultAB = a + b;
+console.log(resultAB);
+
+// +, -, *, /, <, >, ===
+
+let c = 10;
+let d = 20;
+const resultCD = c < d;
+console.log(resultCD);
+
+let i = 5;
+let j = 10;
+const resultIJ = i === j;
+console.log(resultIJ)
+
+// Объяви переменную name и сохрани в ней своё имя. Выведи в console.log.
+// Создай переменные width и height. Найди площадь прямоугольника (width * height).
+// Создай две переменные firstName и lastName. Выведи их вместе.
+
+// Объяви две переменные и сохрани в них числа. Создай третью переменную sum, в которую положи их сумму. Выведи результат.
+// Объяви две переменные и сохрани в них числа. Создай третью переменную product, в которую положи их произведение. Выведи результат.
+// Объяви две переменные и сохрани в них числа. Создай третью переменную quotient, в которую положи результат деления. Выведи результат.
